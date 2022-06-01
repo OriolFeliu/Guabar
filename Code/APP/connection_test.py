f08cb27cd352a07ec393c7f2d72077d3afca0780
OscarMorenoIRamos <106244659+OscarMorenoIRamos@users.noreply.github.com>
2022-06-01 12:46:10 +0200

    Update connection_test.py

--- Code/APP/connection_test.py
+++ Code/APP/connection_test.py
@@ -4,14 +4,14 @@ import paramiko
 class connectionRasp():
 
 	rasp_ip = '192.168.1.131'
-	username = 'ivan'
-	passwd = '4757'
+	username = 'pi'
+	passwd = 'raspi'
 
 	def comandSSH(self,cmd):
 		try:
 			ssh = paramiko.SSHClient()
 			ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-			ssh.connect(self.ip,22,self.user,self.passwd,timeout=5)
+			ssh.connect(self.rasp_ip,22,self.username,self.passwd,timeout=5)
 			stdin,stdout,stderr = ssh.exec_command(cmd)
 			print("OUT: ", stdout.readlines())
 			ssh.close()
